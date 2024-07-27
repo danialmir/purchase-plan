9f1a37a257e9ee5e6ca29f7c54b4f938259f319d
danialmir <mirdanial135@gmail.com>
2024-07-27 16:06:37 +0330

    initializing the project

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { Box } from "@mui/material";
+import "./index.css";
+
+function App() {
+  return (
+    <div className="w-screen h-screen flex items-center justify-center text-4xl ">
+      <span className="block text-7xl pr-5 pb-4 uppercase">wellcome!</span>
+      to the inialized stage of purchse plan project
+    </div>
+  );
+}
+
+export default App;
